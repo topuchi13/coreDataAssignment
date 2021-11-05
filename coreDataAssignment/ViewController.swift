4ead46c8ea7f167b4848d501b14968f1de86372a
topuchi13 <nicolas1213@gmail.com>
2021-11-05 16:06:01 +0400

    Initial Commit

---
+++ coreDataAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataAssignment
+//
+//  Created by Nika Topuria on 05.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
